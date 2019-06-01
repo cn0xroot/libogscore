9f8f5e48dbedc6907a6e13c370eee8cd3066f928
Sukchan Lee <acetcom@gmail.com>
2019-06-01 16:59:12 +0900

    support usrsctp in socknode

--- src/core/ogs-socknode.c
+++ src/core/ogs-socknode.c
@@ -45,8 +45,12 @@ void ogs_socknode_free(ogs_socknode_t *node)
         ogs_pollset_remove(node->pollin.poll);
     if (node->pollout.poll)
         ogs_pollset_remove(node->pollout.poll);
-    if (node->sock)
-        ogs_sock_destroy(node->sock);
+    if (node->sock) {
+        if (node->cleanup)
+            node->cleanup(node->sock);
+        else
+            ogs_sock_destroy(node->sock);
+    }
     ogs_free(node);
 }
 
@@ -238,6 +242,15 @@ int ogs_socknode_fill_scope_id_in_local(ogs_sockaddr_t *sa_list)
     ogs_assert_if_reached();
     return OGS_ERROR;
 #endif
+}
+
+void ogs_socknode_set_cleanup(
+        ogs_socknode_t *node, void (*cleanup)(ogs_sock_t *))
+{
+    ogs_assert(node);
+    ogs_assert(cleanup);
+
+    node->cleanup = cleanup;
 }
 
 void ogs_socknode_set_option(ogs_socknode_t *node, ogs_sockopt_t *option)
--- src/core/ogs-socknode.h
+++ src/core/ogs-socknode.h
@@ -50,6 +50,7 @@ typedef struct ogs_socknode_s {
     ogs_sockaddr_t *addr;
 
     ogs_sock_t *sock;
+    void (*cleanup)(ogs_sock_t *sock);
 
     struct {
         ogs_pollset_t *set;
@@ -74,6 +75,8 @@ int ogs_socknode_probe(
         ogs_list_t *list, ogs_list_t *list6, const char *dev, uint16_t port);
 int ogs_socknode_fill_scope_id_in_local(ogs_sockaddr_t *sa_list);
 
+void ogs_socknode_set_cleanup(
+        ogs_socknode_t *node, void (*cleanup)(ogs_sock_t *));
 void ogs_socknode_set_option(ogs_socknode_t *node, ogs_sockopt_t *option);
 void ogs_socknode_set_poll(ogs_socknode_t *node,
         ogs_pollset_t *set, short when, void *handler, void *data);
