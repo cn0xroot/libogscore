1eaf4abe2c3dcf4d0ff56bff237dbbf474316cda
Harald Welte <laforge@gnumonks.org>
2019-07-18 10:38:59 +0200

    ogs-sha[12]-hmac: Ensure input (key, message) are not modified
    When calling a HMAC API, the input is the key and the message,
    and the output is the generate HMAC.  Functions should not modify
    the input data, as this can lead to all kinds of strange side-effects.
    
    Let's ensure input data are not modified and subsequently mark input
    arguments with 'const' qualifier.

--- src/crypt/ogs-sha1-hmac.c
+++ src/crypt/ogs-sha1-hmac.c
@@ -51,26 +51,24 @@
 
 #include "ogs-crypt.h"
 
-void ogs_hmac_sha1_init(ogs_hmac_sha1_ctx *ctx, uint8_t *key,
+void ogs_hmac_sha1_init(ogs_hmac_sha1_ctx *ctx, const uint8_t *key,
                       uint32_t key_size)
 {
     uint32_t fill;
     uint32_t num;
 
-    uint8_t *key_used;
     uint8_t key_temp[OGS_SHA1_DIGEST_SIZE];
     int i;
 
     if (key_size == OGS_SHA1_BLOCK_SIZE) {
-        key_used = key;
+        memcpy(key_temp, key, sizeof(key_temp));
         num = OGS_SHA1_BLOCK_SIZE;
     } else {
         if (key_size > OGS_SHA1_BLOCK_SIZE){
-            key_used = key_temp;
             num = OGS_SHA1_DIGEST_SIZE;
-            ogs_sha1(key, key_size, key_used);
+            ogs_sha1(key, key_size, key_temp);
         } else { /* key_size > SHA1_BLOCK_SIZE */
-            key_used = key;
+            memcpy(key_temp, key, sizeof(key_temp));
             num = key_size;
         }
         fill = OGS_SHA1_BLOCK_SIZE - num;
@@ -80,8 +78,8 @@ void ogs_hmac_sha1_init(ogs_hmac_sha1_ctx *ctx, uint8_t *key,
     }
 
     for (i = 0; i < num; i++) {
-        ctx->block_ipad[i] = key_used[i] ^ 0x36;
-        ctx->block_opad[i] = key_used[i] ^ 0x5c;
+        ctx->block_ipad[i] = key_temp[i] ^ 0x36;
+        ctx->block_opad[i] = key_temp[i] ^ 0x5c;
     }
 
     ogs_sha1_init(&ctx->ctx_inside);
@@ -106,7 +104,7 @@ void ogs_hmac_sha1_reinit(ogs_hmac_sha1_ctx *ctx)
            sizeof(ogs_sha1_ctx));
 }
 
-void ogs_hmac_sha1_update(ogs_hmac_sha1_ctx *ctx, uint8_t *message,
+void ogs_hmac_sha1_update(ogs_hmac_sha1_ctx *ctx, const uint8_t *message,
                         uint32_t message_len)
 {
     ogs_sha1_update(&ctx->ctx_inside, message, message_len);
@@ -124,8 +122,8 @@ void ogs_hmac_sha1_final(ogs_hmac_sha1_ctx *ctx, uint8_t *mac,
     memcpy(mac, mac_temp, mac_size);
 }
 
-void ogs_hmac_sha1(uint8_t *key, uint32_t key_size,
-          uint8_t *message, uint32_t message_len,
+void ogs_hmac_sha1(const uint8_t *key, uint32_t key_size,
+          const uint8_t *message, uint32_t message_len,
           uint8_t *mac, uint32_t mac_size)
 {
     ogs_hmac_sha1_ctx ctx;
